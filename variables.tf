0c26358be63f73c82646f0427cdf3e06c8f23274
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-11 23:57:50 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type"{}
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami" {}
+variable "key_name" {}
+variable "vpc_security_group_ids" {}
+variable "subnet_id" {}
+variable "Instance_Name" {}
