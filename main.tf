dbe75a0971d2aa6121e71210c864cf227d1e5f06
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-11 23:56:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
-# configure aws provider
+ # configure aws provider
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
 }
 # create instance
 resource "aws_instance" "Deployment4_ec2_2" {
   # input what OS ID you will use
-  ami = "ami-053b0d53c279acc90"
+  ami = var.ami
   # input the instance type according to business needs
-  instance_type = "t2.medium"
+  instance_type = var.instance_type
   # input which subnet the EC2 will be linked to 
-  subnet_id = "subnet-0df0d8d9075062575"
+  subnet_id = var.subnet_id
   # input what security group the EC2 will utilize 
-  vpc_security_group_ids = ["sg-0eec38be6cd7db890"]
+  vpc_security_group_ids = var.vpc_security_group_ids
   # Input which keypair the EC2 will require
-  key_name = "KevinGonzalez623key"
+  key_name = var.key_name
   # This is if the EC2 needs a public IP, by default this is false
   associate_public_ip_address = true
   # This will run "deploy.sh" when the EC2 is created
   user_data = "${file("deploy.sh")}"
   # Because a security group can only be connected to one vpc, when we reference the security group terraform will know which vpc to use 
   tags = {
-    "Name": "Deployment4_ec2_2"
+    "Name": var.Instance_Name
   }
 }
 # request an output of the IP address of the newly created EC2
